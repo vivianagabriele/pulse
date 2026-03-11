d58e2dca551edbbb9cc4ae2c0e3a808cecc21a10
vivianagabriele <bettagabrielle@gmail.com>
2026-03-10 20:04:17 -0700

    Add categories array, LOADING_MSGS, and SYSTEM_PROMPT to constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,10 @@
+const categories = ['category1', 'category2', 'category3'];
+
+const LOADING_MSGS = {
+  loading: 'Loading...',
+  fetching: 'Fetching data...'
+};
+
+const SYSTEM_PROMPT = 'Please wait while we process your request.';
+
+module.exports = { categories, LOADING_MSGS, SYSTEM_PROMPT };
